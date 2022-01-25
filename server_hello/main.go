80f84770393770a6376b6d7ce936e15c18f602b0
shijiao <51071534@qq.com>
2022-01-26 05:16:48 +0800

    feat: grpc + consul + gin

--- server_hello/main.go
+++ server_hello/main.go
@@ -4,7 +4,7 @@ import (
 	pb "cyberblog_go/proto/hello"
 	"cyberblog_go/server_hello/handler"
 	"fmt"
-	consulApi "github.com/hashicorp/consul/api"
+	//consulApi "github.com/hashicorp/consul/api"
 	"google.golang.org/grpc"
 	"log"
 	"net"
@@ -12,8 +12,8 @@ import (
 
 const (
 	PORT = 8080
-	IP   = "192.168.50.143"
-	NAME = "server_hello"
+	IP   = "localhost"
+	//NAME = "server_hello"
 )
 
 func main() {
@@ -22,29 +22,29 @@ func main() {
 		log.Fatalf("failed to listen: %v\n", err)
 	}
 
-	client, err := consulApi.NewClient(consulApi.DefaultConfig())
-	if err != nil {
-		log.Fatalf("consul client: %v\n", err)
-	}
+	//client, err := consulApi.NewClient(consulApi.DefaultConfig())
+	//if err != nil {
+	//	log.Fatalf("consul client: %v\n", err)
+	//}
 
-	reg := consulApi.AgentServiceRegistration{
-		Tags:    []string{NAME},
-		Name:    NAME,
-		Address: IP,
-		Port:    PORT,
-		//Check: &consulApi.AgentServiceCheck{
-		//	CheckID:                        "getimg service test",
-		//	TCP:                            fmt.Sprintf("%s:%d", IP, PORT),
-		//	Timeout:                        "5s",
-		//	Interval:                       "5s",
-		//	DeregisterCriticalServiceAfter: "30s",
-		//},
-	}
+	//reg := consulApi.AgentServiceRegistration{
+	//	Tags:    []string{NAME},
+	//	Name:    NAME,
+	//	Address: IP,
+	//	Port:    PORT,
+	//	//Check: &consulApi.AgentServiceCheck{
+	//	//	CheckID:                        "getimg service test",
+	//	//	TCP:                            fmt.Sprintf("%s:%d", IP, PORT),
+	//	//	Timeout:                        "5s",
+	//	//	Interval:                       "5s",
+	//	//	DeregisterCriticalServiceAfter: "30s",
+	//	//},
+	//}
 
-	err = client.Agent().ServiceRegister(&reg)
-	if err != nil {
-		log.Fatalln("service register fail: ", err)
-	}
+	//err = client.Agent().ServiceRegister(&reg)
+	//if err != nil {
+	//	log.Fatalln("service register fail: ", err)
+	//}
 
 	svc := grpc.NewServer()
 	pb.RegisterGreeterServer(svc, &handler.Server{})
